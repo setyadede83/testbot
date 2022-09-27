1d88c311e0510f483cccf9f1f3a98c6a575040bf
setyadede83 <114272572+setyadede83@users.noreply.github.com>
2022-09-27 18:47:26 +0700

    Add files via upload

--- help.js
+++ help.js
@@ -1,87 +1,90 @@
 exports.allmenu = (prefix) => {
-return `━━━━━━━━━━━━━━━━━━━
-_*OTHER MENU*_
-• ${prefix}ping
-• ${prefix}simi
-• ${prefix}rules
-• ${prefix}listpc
-• ${prefix}listgc
-• ${prefix}daftar
-• ${prefix}owner
-• ${prefix}menfes
-• ${prefix}runtime
-• ${prefix}cariteman
-━━━━━━━━━━━━━━━
-_*TOOLS MENU*_
-• ${prefix}tts
-• ${prefix}isgd
-• ${prefix}cuttly
-• ${prefix}tinyurl
-• ${prefix}nulis
-• ${prefix}toimg
-• ${prefix}obfus
-• ${prefix}ssweb
-• ${prefix}kalkulator
-━━━━━━━━━━━━━━━
-_*SET PROSES/DONE*_
-• ${prefix}setdone
-• ${prefix}changedone
-• ${prefix}delsetdone
-• ${prefix}setproses
-• ${prefix}delsetproses
-• ${prefix}changeproses
-━━━━━━━━━━━━━━━
-_*STORE MENU*_
-• ${prefix}list
-• ${prefix}addlist
-• ${prefix}dellist
-• ${prefix}done
-• ${prefix}proses
-• ${prefix}kali
-• ${prefix}bagi
-• ${prefix}kurang
-• ${prefix}tambah
-━━━━━━━━━━━━━━━
-_*TOPUP GAMES*_
-• ${prefix}topupff 
-• ${prefix}topupml
-━━━━━━━━━━━━━━━
-_*GROUP MENU*_
-• ${prefix}add
-• ${prefix}kick
-• ${prefix}open
-• ${prefix}close
-• ${prefix}group
-• ${prefix}tagall
-• ${prefix}delete
-• ${prefix}revoke
-• ${prefix}antilink
-• ${prefix}hidetag
-• ${prefix}demote
-• ${prefix}setdesc
-• ${prefix}linkgrup
-• ${prefix}promote
-• ${prefix}setppgrup
-• ${prefix}setnamegc
-━━━━━━━━━━━━━━━
-_*OWNER MENU*_
-• ${prefix}bc
-• ${prefix}leave
-• ${prefix}creategc
-• ${prefix}sendsesi
-• ${prefix}setppbot
-• ${prefix}broadcast
-• ${prefix}bugpc *628xxx*
-• ${prefix}buggc *_only grup_*
-• ${prefix}inibug *_only grup_*
-• ${prefix}sendbug *628xxx*
-━━━━━━━━━━━━━━━
-_*DOWNLOADER*_
-• ${prefix}play
-• ${prefix}tiktok
-• ${prefix}ytmp3
-• ${prefix}ytmp4
-• ${prefix}gitclone
-• ${prefix}mediafire
-━━━━━━━━━━━━━━━`
+return `*OTHER MENU*
+ *•* ${prefix}ping
+ *•* ${prefix}simi
+ *•* ${prefix}rules
+ *•* ${prefix}listpc
+ *•* ${prefix}listgc
+ *•* ${prefix}script
+ *•* ${prefix}owner
+ *•* ${prefix}confes
+ *•* ${prefix}menfes
+ *•* ${prefix}runtime
+
+ *TOOLS MENU*
+ *•* ${prefix}tts
+ *•* ${prefix}isgd
+ *•* ${prefix}cuttly
+ *•* ${prefix}tinyurl
+ *•* ${prefix}nulis
+ *•* ${prefix}toimg
+ *•* ${prefix}obfus
+ *•* ${prefix}ssweb
+ *•* ${prefix}kalkulator
+
+ *SET PROSES/DONE*
+ *•* ${prefix}setdone
+ *•* ${prefix}changedone
+ *•* ${prefix}delsetdone
+ *•* ${prefix}setproses
+ *•* ${prefix}delsetproses
+ *•* ${prefix}changeproses
+
+ *STORE MENU*
+ *•* ${prefix}list
+ *•* ${prefix}addlist
+ *•* ${prefix}dellist
+ *•* ${prefix}done
+ *•* ${prefix}proses
+ *•* ${prefix}kali
+ *•* ${prefix}bagi
+ *•* ${prefix}kurang
+ *•* ${prefix}tambah
+
+ *TOPUP GAMES*
+ *•* ${prefix}topupff 
+ *•* ${prefix}topupml
+
+ *GROUP MENU*
+ *•* ${prefix}add
+ *•* ${prefix}kick
+ *•* ${prefix}open
+ *•* ${prefix}close
+ *•* ${prefix}group
+ *•* ${prefix}tagall
+ *•* ${prefix}delete
+ *•* ${prefix}revoke
+ *•* ${prefix}antilink
+ *•* ${prefix}hidetag
+ *•* ${prefix}demote
+ *•* ${prefix}setdesc
+ *•* ${prefix}linkgrup
+ *•* ${prefix}promote
+ *•* ${prefix}setppgrup
+ *•* ${prefix}setnamegc
+
+ *OWNER MENU*
+ *•* ${prefix}bc
+ *•* ${prefix}leave
+ *•* ${prefix}creategc
+ *•* ${prefix}sendsesi
+ *•* ${prefix}setppbot
+ *•* ${prefix}broadcast
+ *•* ${prefix}bugpc *628xxx*
+ *•* ${prefix}sendbug *628xxx*
+
+ *KERANG AJAIB*
+ *•* ${prefix}rate
+ *•* ${prefix}apakah
+ *•* ${prefix}bisakah
+ *•* ${prefix}siapakah
+ *•* ${prefix}bagaimanakah
+
+ *DOWNLOADER*
+ *•* ${prefix}play
+ *•* ${prefix}tiktok
+ *•* ${prefix}ytmp3
+ *•* ${prefix}ytmp4
+ *•* ${prefix}gitclone
+ *•* ${prefix}mediafire`
 }
